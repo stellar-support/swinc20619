978a723a867e230dc462fecaa399dae8c129e185
krishnaa-singh <krishnaa.singh@ksovles.com>
2021-07-12 19:03:06 +0530

    migrated data files and template and controler

--- studio_customization/__manifest__.py
+++ studio_customization/__manifest__.py
@@ -20,11 +20,19 @@
     'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','website_sale','stock','documents_account','product','stock_account', 'purchase'],
+    'depends': ['base','sale','website_sale','stock','documents_account','product','stock_account', 'purchase','delivery','website_sale_delivery'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/res_company.xml',
+        'data/delivery.xml',
+        'data/ir_attachment.xml',
+        'data/ir_cron.xml',
+        'data/mail_template.xml',
+        'data/crm_team.xml',
+        'data/res_company.xml',
+        'data/website.xml',
         'views/assets.xml',
         'views/account_move_inherit.xml',
         'views/res_partner_inherit.xml',
@@ -41,6 +49,9 @@
         'views/delivery_views.xml',
         'views/res_users_views.xml',
         'report/report_dwjo_views.xml',
+        'views/template/website_sale_templates.xml',
+        'views/template/payment_templates.xml',
+        'views/template/portal_templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
